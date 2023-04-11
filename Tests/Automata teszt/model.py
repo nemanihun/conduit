8ee8bd835a3c1daf87d909e0380dc0f7e6d648a6
Nemanihun <nanweblinkek@gmail.com>
2023-04-11 13:22:16 +0200

    Modifications 2023/04/11

--- Tests/Automata teszt/model.py
+++ Tests/Automata teszt/model.py
@@ -1,11 +1,7 @@
 import time
 
-import configuration as config
 import data as adatok
-import test_conduit
-from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
@@ -21,38 +17,22 @@ class Testcase:
         test_name = self.test_name
         print(test_name)
 
-    def teszt_start(self):
-        test_name = self.test_name
-
-        # Teszteset elindul
-        print()
-        print(f'{test_name} teszteset elindult')
-        print()
-
-    def expected_res(self, asserts):
+    def expected_res(self):
         # Teszteset eredmény vizsgálata
         print()
         print('Teszt eset eredményének vizsgálata')
         print()
-
         print(f'Elvárt eredmény: {self.expected_result}')
         print()
 
-        assert asserts
-
     def actual_res(self):
-        print(f'{self.test_name} teszteset sikeres')
+        print(f'{self.test_name} teszteset sikeresen lefutott')
         print()
-
         print(f'Aktuális eredmény:  {self.actual_result}')
-        print()
-
-        print(f'{self.test_name} teszteset sikeresen lezárult')
-        print()
 
 
 class GetUsers:
-
+    # Funkciók, amelyek regisztrációval, belépéssel kapcsolatosak.
     def __init__(self):
         self.user_data = []
 
@@ -70,15 +50,16 @@ class GetUsers:
             users = adatok.Users.bad_user_password.values()
         if us == 'user4':
             users = adatok.Users.user4_data.values()
+        if us == 'user5':
+            users = adatok.Users.user5_data.values()
 
         for user in users:
             self.user_data.append(user)
 
     # Regisztráció
-    def signup(self, browser, us):
+    def signup(self, browser, user):
 
-        # browser.get(adatok.Urls.home_url)
-        self.userdata(us)
+        self.userdata(user)
 
         signup_link = browser.find_element(By.LINK_TEXT, 'Sign up')
         signup_link.click()
@@ -108,22 +89,22 @@ class GetUsers:
         ok_btn.click()
 
     # Bejelentkezés
-    def signin(self, browser, us):
+    def signin(self, browser, user):
 
-        self.userdata(us)
+        self.userdata(user)
 
         signin_link = browser.find_element(By.LINK_TEXT, 'Sign in')
         signin_link.click()
-
-        print('Bejelentkezési adatok megadása')
 
         email = browser.find_elements(By.XPATH, '//input')[0]
         email.send_keys(self.user_data[1])
 
         password = browser.find_elements(By.XPATH, '//input')[1]
         password.send_keys(self.user_data[2])
-
-        print(f'Bejelentkezési adatok: Email cím: {self.user_data[1]}, Jelszó: {self.user_data[2]}')
+        print()
+        print('Bejelentkezem az alkalmazásba')
+        print()
+        print(f'Megadom a bejelentkezési adatokat: Email cím: {self.user_data[1]}, Jelszó: {self.user_data[2]}')
 
         submit_btn = browser.find_element(By.XPATH, '//button')
         submit_btn.click()
@@ -221,3 +202,214 @@ class Cookie:
 
         print()
         print(f'Aktuális eredmény: Eltűnt a "cookie_bar".')
+
+
+class ManipulatePages:
+    # Funkciók, amelyek az oldalak tartalmát dolgozzák fel, módosítják vagy ezekhez szükségesek.
+    def __init__(self):
+        self.article_amount = 0
+        self.article_list = []
+        self.article_data = []
+
+    # Általános funkciók
+
+    # Cikk adatok elérése a data.py-ból
+    def articledata(self, adata):
+
+        articles = ''
+
+        if adata == 'article1_data':
+            articles = adatok.Articles.article1_data.values()
+        if adata == 'article2_data':
+            articles = adatok.Articles.article2_data.values()
+        if adata == 'article3_data':
+            articles = adatok.Articles.article3_data.values()
+        for article in articles:
+            self.article_data.append(article)
+
+    # Profil oldalra navigálás
+    def go_to_profile(self, browser):
+        time.sleep(2)
+        nav_links = browser.find_elements(By.XPATH, '//nav/div/ul/li[@class="nav-item"]')
+        nav_links[3].click()
+        time.sleep(60)
+
+    # Cikkek száma a lista oldalon
+    def article_number(self, browser):
+
+        article_list = browser.find_elements(By.CLASS_NAME, 'article-preview')
+        self.article_list = article_list
+
+        article_number = len(article_list)
+        self.article_amount = article_number
+
+        return article_number
+
+    # Egy konkrét cikk megtalálása és megnyitása
+    def find_article(self, browser, article):
+        self.articledata(article)
+        article_data = self.article_data
+        pages = WebDriverWait(browser, 5).until(
+            EC.presence_of_all_elements_located((By.XPATH, '//a[@class="page-link"]')))
+        print(pages)
+
+        next_page = 0
+
+        titles = WebDriverWait(browser, 5).until(
+            EC.presence_of_all_elements_located((By.XPATH, '//a[@class="preview-link"]/h1')))
+
+        # browser.refresh()
+
+        # for page in pages:
+
+        for title in titles:
+
+            if title.text == article_data[0]:
+                # next_page += 1
+                title.click()
+
+            # next_page += 1
+
+            # if next_page == 0:
+            #     page.click()
+
+    # Tesztesetek funkciói
+
+    # Cikkek listázása
+    def article_listing(self, browser):
+        self.article_number(browser)
+        amount = self.article_amount
+        counter = 0
+
+        title_list = browser.find_elements(By.XPATH, '//h1')
+        print()
+        print('Kilistázom a cikkek címét')
+        print()
+        print('Az oldalon megjelenő cikkek címei:')
+        print()
+        for elem in title_list[1:]:
+            counter += 1
+            print(f'{counter}. Cikk címe: {elem.text}')
+        print()
+        page_url = browser.current_url
+        print(f'Cikkek száma a {page_url} URL című oldalon: {amount}')
+        print()
+
+    # Több oldalas listán megjelenő cikkek listázása
+    # Több oldalas lista bejárása
+    def multi_page_list_explore(self, browser):
+        pages = browser.find_elements(By.XPATH, '//a[@class="page-link"]')
+        total_pages = len(pages)
+        counter = 0
+        print()
+        print(f'Lista oldalak száma {total_pages}')
+        for article in range(total_pages):
+            counter += 1
+            print(f'Cikkek a {counter}. oldalon:')
+            print()
+            self.article_listing(browser)
+
+            if counter < total_pages:
+                print()
+                print('Tovább lépek a lista következő oldalára.')
+                next_link = pages[counter]
+                next_link.click()
+                print()
+            else:
+                print()
+                print('Elértem az utolsó lista oldal végére.')
+                print()
+
+    # Új cikk feltöltése
+    def new_article_upload(self, browser, article):
+        self.articledata(article)
+
+        new_article_btn = browser.find_element(By.CLASS_NAME, 'ion-compose')
+        new_article_btn.click()
+
+        WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.XPATH, '//input')))
+
+        print('Új cikk adatok megadása')
+
+        input_fields = browser.find_elements(By.XPATH, '//input')
+
+        article_title = input_fields[0]
+        article_title.send_keys(self.article_data[0])
+
+        article_about = input_fields[1]
+        article_about.send_keys(self.article_data[1])
+
+        article_content = browser.find_element(By.XPATH, '//textarea')
+        article_content.send_keys(self.article_data[2])
+
+        article_tag = input_fields[2]
+        article_tag.send_keys(self.article_data[3])
+
+        print(
+            f'Cikk adatai: Cikk címe: {self.article_data[0]}, Cikk témája: {self.article_data[1]}, Cikk szövege: {self.article_data[2]}, Cikk címkéi {self.article_data[2]}')
+
+        publish_btn = browser.find_element(By.XPATH, '//button')
+        publish_btn.click()
+
+    # Cikk módosítása
+    def modify_article(self, browser, article):
+        self.articledata(article)
+
+        self.go_to_profile(browser)
+
+        print()
+        print('Megkeresem a módosítandó cikket.')
+        print()
+
+        self.find_article(browser, article)
+
+        print('Rákattintok az "Edit article" szerkesztés gombra')
+        print()
+
+        edit_btn = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.CLASS_NAME, 'ion-edit')))
+        edit_btn.click()
+
+        print('Módosított cikk adatok megadása')
+
+        self.article_data = []
+        self.articledata('article2_data')
+        print(self.article_data)
+
+        input_fields = WebDriverWait(browser, 5).until(EC.presence_of_all_elements_located((By.XPATH, '//input')))
+        print(input_fields)
+
+        article_title = input_fields[0]
+        article_title.clear()
+        article_title.send_keys(self.article_data[0])
+
+        article_about = input_fields[1]
+        article_about.clear()
+        article_about.send_keys(self.article_data[1])
+
+        article_content = browser.find_element(By.XPATH, '//textarea')
+        article_content.clear()
+        article_content.send_keys(self.article_data[2])
+
+        print(
+            f'Cikk adatai: Cikk címe: {self.article_data[0]}, Cikk témája: {self.article_data[1]}, Cikk szövege: {self.article_data[2]}, Cikk címkéi {self.article_data[2]}')
+
+        publish_btn = browser.find_element(By.XPATH, '//button')
+        publish_btn.click()
+
+    # Cikk törlése
+    def delete_article(self, browser, article):
+        self.articledata(article)
+
+        self.go_to_profile(browser)
+
+        print()
+        print('Megkeresem a törlendő cikket.')
+        print()
+
+        self.find_article(browser, article)
+
+        print('Rákattintok az "Delete article" törlés gombra')
+        print()
+
+        delete_btn = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.CLASS_NAME, 'ion-trash-a')))
+        delete_btn.click()
--- Tests/Automata teszt/test_conduit.py
+++ Tests/Automata teszt/test_conduit.py
@@ -1,14 +1,9 @@
 import data as adatok
 import configuration as config
 import model as model
+import asserts as asserting
 import time
 
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver import Chrome
-from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.relative_locator import locate_with
 from selenium.webdriver.support import expected_conditions as EC
@@ -22,20 +17,20 @@ class TestConduit(object):
     def setup_method(self):
         self.browser = config.get_preconfigured_chrome_driver()
         self.browser.get(adatok.Urls.home_url)
+        self.browser.maximize_window()
         time.sleep(10)
 
     def teardown_method(self):
         self.browser.quit()
 
-    # TestCase 01 - Regisztráció
+    # ATC 01 - Regisztráció
     def test_signup(self):
         getusers = model.GetUsers()
 
         required_data = {
-            'test_name': 'TestCase 01 - Regisztráció',
+            'test_name': 'ATC 01 - Regisztráció',
             'expected_result': 'Sikeres regisztráció után látszik az "Ok" gomb',
             'actual_result': 'A regisztráció sikeres, látszik az "Ok" gomb',
-
         }
 
         testcase1 = model.Testcase(
@@ -45,67 +40,73 @@ class TestConduit(object):
         )
 
         testcase1.teszt_nev()
-        testcase1.teszt_start()
 
         getusers.signup(self.browser, 'user1')
+
+        asserts = asserting.Asserts()
+
         WebDriverWait(self.browser, 5).until(
             EC.presence_of_element_located((By.CLASS_NAME, 'ion-android-exit')))
 
         getusers.logout_btn(self.browser).is_displayed()
-        testcase1.expected_res(getusers.signup_ok_btn(self.browser).is_displayed())
+
+        testcase1.expected_res()
+
+        asserts.general_asserts(getusers.signup_ok_btn(self.browser).is_displayed())
+
         testcase1.actual_res()
 
-    # TestCase 02 - Bejelentkezés
+    # ATC 02 - Bejelentkezés
     def test_signin(self):
         getusers = model.GetUsers()
 
         required_data = {
-            'test_name': 'TestCase 02 - Bejelentkezés',
+            'test_name': 'ATC 02 - Bejelentkezés',
             'expected_result': 'Sikeres bejelentkezés után látszik a "Log out" gomb',
             'actual_result': 'Sikeres bejelentkezés, látszik a "Log out" gomb',
-
         }
 
         testcase2 = model.Testcase(
             test_name=required_data['test_name'],
             expected_result=required_data['expected_result'],
             actual_result=required_data['actual_result'],
-
         )
 
         testcase2.teszt_nev()
-        testcase2.teszt_start()
 
         getusers.signin(self.browser, 'user1')
 
+        asserts = asserting.Asserts()
+
         WebDriverWait(self.browser, 5).until((EC.url_to_be(adatok.Urls.home_url)))
 
-        testcase2.expected_res(getusers.logout_btn(self.browser).is_displayed())
+        testcase2.expected_res()
+
+        asserts.general_asserts(getusers.logout_btn(self.browser).is_displayed())
 
         testcase2.actual_res()
 
-    # TestCase 03 - Kijelentkezés
+    # ATC 03 - Kijelentkezés
     def test_logout(self):
         getusers = model.GetUsers()
 
         required_data = {
-            'test_name': 'TestCase 03 - Kijelentkezés',
+            'test_name': 'ATC 03 - Kijelentkezés',
             'expected_result': 'Sikeres kijelentkezés után látszik a "Sign in" gomb',
             'actual_result': 'Sikeres kijelentkezés, látszik a "Sign in" gomb',
-
         }
 
         testcase3 = model.Testcase(
             test_name=required_data['test_name'],
             expected_result=required_data['expected_result'],
             actual_result=required_data['actual_result'],
-
         )
 
         testcase3.teszt_nev()
-        testcase3.teszt_start()
 
         getusers.signin(self.browser, 'user1')
+
+        asserts = asserting.Asserts()
 
         WebDriverWait(self.browser, 10).until(
             EC.presence_of_element_located((By.CLASS_NAME, 'ion-android-exit')))
@@ -114,28 +115,211 @@ class TestConduit(object):
 
         time.sleep(5)
 
-        testcase3.expected_res(getusers.signin_btn(self.browser).is_displayed())
+        testcase3.expected_res()
+
+        asserts.general_asserts(getusers.signin_btn(self.browser).is_displayed())
+
         testcase3.actual_res()
 
-    # TestCase 04 - Adatkezelési nyilatkozat használata
+    # ATC 04 - Adatkezelési nyilatkozat használata
     def test_cookie_handling(self):
         cookie = model.Cookie()
 
         required_data = {
-            'test_name': 'TestCase 04 - Adatkezelési nyilatkozat használata',
+            'test_name': 'ATC 04 - Adatkezelési nyilatkozat használata',
             'expected_result': '',
             'actual_result': '',
-
         }
 
         testcase4 = model.Testcase(
             test_name=required_data['test_name'],
             expected_result=required_data['expected_result'],
             actual_result=required_data['actual_result'],
-
         )
 
         testcase4.teszt_nev()
-        testcase4.teszt_start()
 
         cookie.cookie_handling(self.browser)
+
+    # ATC 05 - Adatok listázása
+    def test_data_listing(self):
+        manipulatepages = model.ManipulatePages()
+
+        getusers = model.GetUsers()
+
+        asserts = asserting.Asserts()
+
+        required_data = {
+            'test_name': 'ATC 05 - Adatok listázása',
+            'expected_result': 'Legalább egy cikk van az oldalon',
+            'actual_result': 'Található cikk az oldalon',
+        }
+
+        testcase5 = model.Testcase(
+            test_name=required_data['test_name'],
+            expected_result=required_data['expected_result'],
+            actual_result=required_data['actual_result'],
+        )
+
+        testcase5.teszt_nev()
+
+        getusers.signin(self.browser, 'user1')
+
+        WebDriverWait(self.browser, 15).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'counter')))
+
+        testcase5.expected_res()
+
+        asserts.article_listing_assert(self.browser)
+
+        manipulatepages.article_listing(self.browser)
+
+        testcase5.actual_res()
+
+        # ATC 06 - Több oldalas lista bejárása
+
+    def test_multi_page_listing(self):
+        manipulatepages = model.ManipulatePages()
+
+        getusers = model.GetUsers()
+
+        asserts = asserting.Asserts()
+
+        required_data = {
+            'test_name': 'ATC 06 - Több oldalas lista bejárása',
+            'expected_result': 'Ki listázom az összes oldalon található cikk címét és elérek a lista oldalak végére, ezzel bejárva az összes lista oldalt.',
+            'actual_result': 'Elérek a lista oldalak végére, bejártam a lista oldalát, ki tudtam listázni az összes cikk címét a lista oldalakról.',
+        }
+
+        testcase6 = model.Testcase(
+            test_name=required_data['test_name'],
+            expected_result=required_data['expected_result'],
+            actual_result=required_data['actual_result'],
+        )
+
+        testcase6.teszt_nev()
+
+        getusers.signin(self.browser, 'user1')
+
+        WebDriverWait(self.browser, 15).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'counter')))
+
+        time.sleep(5)
+
+        manipulatepages.multi_page_list_explore(self.browser)
+
+        testcase6.expected_res()
+
+        asserts.multi_page_listing_assert(self.browser)
+
+        # ATC 07 - Új adat bevitel / Új cikk feltöltése
+
+    def test_new_data_upload(self):
+        manipulatepages = model.ManipulatePages()
+
+        getusers = model.GetUsers()
+
+        asserts = asserting.Asserts()
+
+        required_data = {
+            'test_name': 'ATC 07 - Új adat bevitele',
+            'expected_result': 'Az új cikk feltöltése sikeres. A cikk címe megjelenik az oldalon.',
+            'actual_result': 'Megjelenik a cikk címe. A cikk felvitele sikeres volt.',
+            'article': 'article1_data'
+        }
+
+        article = required_data['article']
+
+        testcase7 = model.Testcase(
+            test_name=required_data['test_name'],
+            expected_result=required_data['expected_result'],
+            actual_result=required_data['actual_result'],
+        )
+
+        testcase7.teszt_nev()
+
+        getusers.signin(self.browser, 'user1')
+
+        WebDriverWait(self.browser, 15).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'counter')))
+
+        manipulatepages.new_article_upload(self.browser, article)
+
+        testcase7.expected_res()
+
+        asserts.article_assert(self.browser, article)
+
+        testcase7.actual_res()
+
+        # ATC 08 - Adat módosítása / Cikk módosítása
+
+    def test_modify_data(self):
+        manipulatepages = model.ManipulatePages()
+
+        getusers = model.GetUsers()
+
+        asserts = asserting.Asserts()
+
+        required_data = {
+            'test_name': 'ATC 08 - Adat módosítása/Cikk módosítása',
+            'expected_result': 'A cikk módosítása sikeres. A cikk címe megjelenik az oldalon.',
+            'actual_result': 'Megjelenik a módosított cikk címe. A cikk módosítása sikeres volt.',
+            'article': 'article1_data'
+        }
+
+        article = required_data['article']
+
+        testcase8 = model.Testcase(
+            test_name=required_data['test_name'],
+            expected_result=required_data['expected_result'],
+            actual_result=required_data['actual_result'],
+        )
+
+        testcase8.teszt_nev()
+
+        getusers.signin(self.browser, 'user1')
+
+        WebDriverWait(self.browser, 5).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'counter')))
+
+        manipulatepages.modify_article(self.browser, article)
+
+        testcase8.expected_res()
+
+        asserts.article_assert(self.browser, 'article2_data')
+
+        testcase8.actual_res()
+
+        # ATC 09 - Adat törlése / Cikk törlése
+
+    def test_delete_data(self):
+        manipulatepages = model.ManipulatePages()
+
+        getusers = model.GetUsers()
+
+        asserts = asserting.Asserts()
+
+        required_data = {
+            'test_name': 'ATC 09 - Adat törlése/Cikk törlése',
+            'expected_result': 'A cikk törlése sikeres. A cikk nem jelenik már meg az oldalon.',
+            'actual_result': 'A cikk törlése sikeres. A cikk már nem jelenik meg az oldalon.',
+            'article': 'article2_data'
+        }
+
+        article = required_data['article']
+
+        testcase9 = model.Testcase(
+            test_name=required_data['test_name'],
+            expected_result=required_data['expected_result'],
+            actual_result=required_data['actual_result'],
+        )
+
+        testcase9.teszt_nev()
+
+        getusers.signin(self.browser, 'user1')
+
+        WebDriverWait(self.browser, 5).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'counter')))
+
+        manipulatepages.delete_article(self.browser, article)
+
+        testcase9.expected_res()
+
+        asserts.delete_article_assert(self.browser, article)
+
+        testcase9.actual_res()
